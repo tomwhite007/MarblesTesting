29cb8368d32b4e471eab350f8315889ba2a495c6
Tom White <tomwhite007@gmail.com>
2019-08-21 10:37:02 +0100

    basic marbles test and flush

--- apps/marbles-testing/src/app/app.component.ts
+++ apps/marbles-testing/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DummyService } from './services/dummy.service';
+import { inherits } from 'util';
 
 @Component({
   selector: 'marbles-testing-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private dummy: DummyService) {}
   title = 'marbles-testing';
+
+  ngOnInit() {
+    this.dummy.resetCounter();
+    this.dummy.pushInAndLogToConsole();
+  }
 }
