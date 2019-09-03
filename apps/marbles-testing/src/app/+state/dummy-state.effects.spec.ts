81e872033ad870b883cfe0c3a447f5705e48ff15
Tom White <tomwhite007@gmail.com>
2019-09-03 09:10:38 +0100

    proper jest mocked service

--- apps/marbles-testing/src/app/+state/dummy-state.effects.spec.ts
+++ apps/marbles-testing/src/app/+state/dummy-state.effects.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule, Action } from '@ngrx/store';
@@ -9,12 +9,25 @@ import { provideMockActions } from '@ngrx/effects/testing';
 import { NxModule } from '@nrwl/angular';
 
 import { DummyStateEffects } from './dummy-state.effects';
-import { LoadDummyState, DummyStateLoaded } from './dummy-state.actions';
+import {
+  LoadDummyState,
+  DummyStateLoaded,
+  DummyStateLoadError
+} from './dummy-state.actions';
 import { TestScheduler } from 'rxjs/testing';
+import { FakeRestService } from '../services/fake-rest.service';
 
 describe('DummyStateEffects', () => {
   let actions$: Observable<Action>;
   let effects: DummyStateEffects;
+  const apiSpy = {
+    getDummyData: jest.fn(() => of(['Mocked']))
+  };
+  const testScheduler = new TestScheduler((actual, expected) => {
+    // asserting the two objects are equal
+    // required for all Marbles assertions to be asserted by Jest or Jasmine
+    expect(actual).toEqual(expected);
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -23,27 +36,37 @@ describe('DummyStateEffects', () => {
         StoreModule.forRoot({}),
         EffectsModule.forRoot([])
       ],
-      providers: [DummyStateEffects, provideMockActions(() => actions$)]
+      providers: [
+        DummyStateEffects,
+        provideMockActions(() => actions$),
+        { provide: FakeRestService, useValue: apiSpy }
+      ]
     });
 
     effects = TestBed.get(DummyStateEffects);
   });
 
-  describe('loadDummyState$', () => {
-    const testScheduler = new TestScheduler((actual, expected) => {
-      // asserting the two objects are equal
-      // required for all Marbles assertions to be asserted by Jest or Jasmine
-      expect(actual).toEqual(expected);
-    });
+  it('should trigger loaded action', () => {
+    testScheduler.run(({ cold, hot, expectObservable, flush }) => {
+      actions$ = hot('-a-|', { a: new LoadDummyState() });
 
-    it('should trigger loaded action', () => {
-      testScheduler.run(({ cold, hot, expectObservable, flush }) => {
-        actions$ = hot('-a-|', { a: new LoadDummyState() });
-
-        expectObservable(effects.loadDummyState$).toBe('-a-|', {
-          a: new DummyStateLoaded(['test1', 'test2', 'test3'])
-        });
+      expectObservable(effects.loadDummyState$).toBe('-a-|', {
+        a: new DummyStateLoaded(['Mocked'])
       });
     });
   });
+
+  // it('should trigger load error action', () => {
+  //   testScheduler.run(({ cold, hot, expectObservable, flush }) => {
+  //     const dummyApiError = new Error('Oops! Api request failed');
+  //     apiSpy.getDummyData.mockImplementation(() => throwError(dummyApiError));
+
+  //     actions$ = hot('-a-|', { a: new LoadDummyState() });
+
+  //     expectObservable(effects.loadDummyState$).toBe('----a-|', {
+  //       a: new DummyStateLoadError(dummyApiError)
+  //     });
+  //     // effects.loadDummyState$.subscribe(res => console.log(res));
+  //   });
+  // });
 });
