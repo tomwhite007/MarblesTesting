5c2074c1b07e3c0e8bc2e49fddb646517585e2e8
Tom White <tomwhite007@gmail.com>
2019-12-11 08:46:13 +0000

    Removed orphaned selector tests

--- apps/marbles-testing/src/app/+state/dummy-state.selectors.spec.ts
+++ apps/marbles-testing/src/app/+state/dummy-state.selectors.spec.ts
@@ -1,47 +1,22 @@
-import { Entity, DummyStateState } from './dummy-state.reducer';
 import { dummyStateQuery } from './dummy-state.selectors';
 
 describe('DummyState Selectors', () => {
   const ERROR_MSG = 'No Error Available';
-  const getDummyStateId = it => it['id'];
 
   let storeState;
 
   beforeEach(() => {
-    const createDummyState = (id: string, name = ''): Entity => ({
-      id,
-      name: name || `name-${id}`
-    });
     storeState = {
       dummyState: {
-        list: [
-          createDummyState('PRODUCT-AAA'),
-          createDummyState('PRODUCT-BBB'),
-          createDummyState('PRODUCT-CCC')
-        ],
-        selectedId: 'PRODUCT-BBB',
-        error: ERROR_MSG,
-        loaded: true
+        list: [],
+        selectedId: null,
+        loaded: true,
+        error: ERROR_MSG
       }
     };
   });
 
   describe('DummyState Selectors', () => {
-    it('getAllDummyState() should return the list of DummyState', () => {
-      const results = dummyStateQuery.getAllDummyState(storeState);
-      const selId = getDummyStateId(results[1]);
-
-      expect(results.length).toBe(3);
-      expect(selId).toBe('PRODUCT-BBB');
-    });
-
-    it('getSelectedDummyState() should return the selected Entity', () => {
-      const result = dummyStateQuery.getSelectedDummyState(storeState);
-      const selId = getDummyStateId(result);
-
-      expect(selId).toBe('PRODUCT-BBB');
-    });
-
     it("getLoaded() should return the current 'loaded' status", () => {
       const result = dummyStateQuery.getLoaded(storeState);
 
